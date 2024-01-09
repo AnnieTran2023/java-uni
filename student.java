54cce3ec5ec61329ca56dd8ff7f1f3f075662e9c
Annie Tran <132701766+AnnieTran2023@users.noreply.github.com>
2024-01-09 20:59:16 +0200

    student class

--- student.java
+++ student.java
@@ -1,1 +1,57 @@
+import java.math.BigDecimal;
+
+public class Student {
+
+	private String name;
+	private int[] score;
+
+	public Student(String name, int[] score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public int getNumberOfScore() {
+		return score.length;
+	}
+
+	int sum = 0;
+
+	public int getTotal() {
+		for (int s : score) {
+			sum += s;
+		}
+		return sum;
+	}
+
+	public int getMax() {
+		int max = score[0];
+		for (int s : score) {
+			if (s > max) {
+				max = s;
+			}
+		}
+		return max;
+	}
+
+	public BigDecimal getAverageMarks() {
+		// TODO Auto-generated method stub
+		int sum = getTotal();
+		int number = score.length;
+		return new BigDecimal(sum).divide(new BigDecimal(number));
+	}
+}
+
+public class StudentRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] score = { 100, 89, 90, 99 };
+		Student student = new Student("Julie", score);
+		System.out.println(student.getMax());
+
+		BigDecimal average = student.getAverageMarks();
+		System.out.println(average);
+	}
+
+}
 
