ecf1351a3ff1edfbc37222bde2a86b8cbbf13c04
Annie Tran <132701766+AnnieTran2023@users.noreply.github.com>
2024-01-11 21:37:06 +0200

    Update array.java

--- array.java
+++ array.java
@@ -1,1 +1,29 @@
+//calculate sum of each array and compare if they are equal
 
+public class BiArray {
+
+    private int[] array1;
+    private int[] array2;
+
+    public BiArray(int[] array1, int[] array2) {
+        this.array1 = array1;
+        this.array2 = array2;
+    }
+   
+    public boolean areSumsEqual() {
+
+        int sum1 = calculateSum(array1);
+        int sum2 = calculateSum(array2);
+        return sum1==sum2;
+    }
+
+    private int calculateSum(int[] array) {
+        
+        // TODO: Write the code to calculate the sum of the array.
+          int sum = 0;
+        for(int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+}
